6ee9b1e4db37f8213e8c6aafd17046e2d9ec8e50
zjotaro <zzc5464@foxmail.com>
2021-10-29 14:39:13 +0800

    feat: 添加travis

--- bin/index.js
+++ bin/index.js
@@ -1,36 +1,40 @@
 #!/usr/bin/env node
-process.title = 'trans';
-
-const commander = require('commander')
-const trans = require('../src/index')
-const Path = require('path')
-
-commander
-.version(require('../package').version)
-.usage('<command> [options]')
-.command('generate', 'generate file from a template (short-cut alias: "g")')
-
+process.title = "trans";
+// npx transi18n xtoj ./src/_static_/local.xlsx ./src/locales
+const commander = require("commander");
+const trans = require("../src/index");
+const Path = require("path");
 
 commander
-  .command('jtox <localeFolder> [xsltPath]')
-  .description('locale js folder to excel', {
-    localeFolder: 'locale js folder ex: ./locale',
-    xsltPath: 'xslt file path  ex: a/b.xlsx'
+  .version(require("../package").version)
+  .usage("<command> [options]")
+  .command("generate", 'generate file from a template (short-cut alias: "g")');
+
+commander
+  .command("jtox <localeFolder> [xsltPath]")
+  .description("locale js folder to excel", {
+    localeFolder: "locale js folder ex: ./locale",
+    xsltPath: "xslt file path  ex: a/b.xlsx",
   })
   .action((localeFolder, xsltPath) => {
-    trans.jsToExcel(Path.join(process.cwd(), localeFolder), Path.join(process.cwd(), xsltPath))
-    console.log('js translate excel done');
+    trans.jsToExcel(
+      Path.join(process.cwd(), localeFolder),
+      Path.join(process.cwd(), xsltPath)
+    );
+    console.log("js translate excel done");
   });
 
 commander
-  .command('xtoj <xsltPath> [localeFolder]')
-  .description('locale js folder to excel', {
-    localeFolder: 'locale js folder ex: ./locale',
-    xsltPath: 'xslt file path  ex: a/b.xlsx'
+  .command("xtoj <xsltPath> [localeFolder]")
+  .description("locale js folder to excel", {
+    localeFolder: "locale js folder ex: ./locale",
+    xsltPath: "xslt file path  ex: a/b.xlsx",
   })
   .action((xsltPath, localeFolder) => {
-    trans.excelToJs(Path.join(process.cwd(), xsltPath), Path.join(process.cwd(), localeFolder))
-    console.log('js translate excel done');
+    trans.excelToJs(
+      Path.join(process.cwd(), xsltPath),
+      Path.join(process.cwd(), localeFolder)
+    );
+    console.log("js translate excel done");
   });
-commander.parse(process.argv)
-
+commander.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // import config from './config'
 // import { excelToJs, jsToExcel } from './utils/fs'
-const utils = require('./utils/fs')
+const utils = require("./utils/fs");
 
 // excelToJs(config.DISTFILEPATH,config.JSPATH)
 
 // jsToExcel(config.READFLODER, config.DISTFILEPATH)
-exports.excelToJs = utils.excelToJs
-exports.jsToExcel = utils.jsToExcel
+exports.excelToJs = utils.excelToJs;
+exports.jsToExcel = utils.jsToExcel;
