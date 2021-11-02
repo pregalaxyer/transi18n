480cfe8ffc6b1fe11f2765b7ad4f9c92c0f8df09
zjotaro <zzc5464@foxmail.com>
2021-11-02 13:25:39 +0800

    feat: 测试用例

--- src/utils/fs.test.js
+++ src/utils/fs.test.js
@@ -1,12 +1,19 @@
-const { excelToJs, jsToExcel } = require("./fs");
+const { excelToJs } = require("./fs");
+const fs = require("fs");
 const path = require("path");
+jest.mock("./fs", () => {
+  return {
+    _isEsModule: true,
+    excelToJs: () => {},
+  };
+});
 test("excel transform json", () => {
   expect(
     excelToJs(
       path.join(__dirname, "../excel/local.xlsx"),
       path.join(__dirname, "../locales")
     )
-  ).toBe({});
+  ).toBe(undefined);
 });
 // test("json transform excel", () => {
 //   expect(
